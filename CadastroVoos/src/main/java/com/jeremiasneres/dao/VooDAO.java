fa94086eaee7978b2423c07eecde34b584ac761b
Jeremias Neres <jeremiasneres23@gmail.com>
2019-06-27 11:05:36 -0300

    Configurando metodos PUT e DELETE

--- CadastroVoos/src/main/java/com/jeremiasneres/dao/VooDAO.java
+++ CadastroVoos/src/main/java/com/jeremiasneres/dao/VooDAO.java
@@ -27,8 +27,8 @@ public class VooDAO extends JpaDAO<Voo> {
         getEm().persist(entity);
     }
 
-    public boolean remove(Voo voo) {
+    /*public boolean remove(Voo voo) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    }*/
 
 }
